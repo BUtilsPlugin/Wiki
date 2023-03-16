75571f8dc0bc3c7460720515b60d5985028ff752
Lucas <accounts@banko.tv>
2023-03-17 00:11:34 +0100

    Remove unnecessary header

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,11 +27,8 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
     return (
-        <Layout
-            title={`The ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
+        <Layout>
             <HomepageHeader />
             <main>
                 <HomepageFeatures />
