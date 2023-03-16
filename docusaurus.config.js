68f0eb06ab32221580aa6597e96f1fa926473e02
Lucas <accounts@banko.tv>
2023-03-16 23:33:27 +0100

    Implement Github Actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'BUtilsPlugin', // Usually your GitHub org/user name.
   projectName: 'Wiki', // Usually your repo name.
+  deploymentBranch: 'master',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
