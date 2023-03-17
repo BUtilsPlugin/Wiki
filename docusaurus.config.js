d8b600a24ec8299b4458040d9e2987ad7ba745f5
Lucas <accounts@banko.tv>
2023-03-17 10:53:39 +0100

    Fix editUrls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/BUtilsPlugin/butils-wiki/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/BUtilsPlugin/Wiki/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/BUtilsPlugin/butils-wiki/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/BUtilsPlugin/Wiki/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
